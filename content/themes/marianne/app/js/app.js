c109ab07d7984ed5694531793a2a22a525f67bd3
sckyzopat <mariannexpiquet@gmail.com>
2019-07-30 18:32:03 +0200

    problem menu toggle - not active

--- content/themes/marianne/app/js/app.js
+++ content/themes/marianne/app/js/app.js
@@ -1,8 +1,25 @@
 app = {
 
+    $body: null,
+
     init: function() {
         console.log('init');
+
+        app.$body = $('body');
+
+        // Je dépose un event click sur mes boutons ui-button
+        $('.ui-button').on('click', app.handleToggleMenu);
+    },
+
+    handleToggleMenu: function(evt) {
+
+        console.log('app.handle');
+
+        // J'évite le rechargement de la page
+        evt.preventDefault();
+
+        app.$body.toggleClass('menu-visible');
     }
-}
+};
 
 $(app.init);
